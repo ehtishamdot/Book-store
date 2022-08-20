5b62200da7891355bbf35326977f7aa6ded6ab63
ehtishamdot <ahtionpc1@gmail.com>
2022-08-20 20:39:07 +0500

    Frontend setup added

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "./component/Login";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Login />
+    </React.Fragment>
+  );
+};
+
+export default App;
---
+++ frontend/src/component/Bookstore.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bookstore = () => {
+  return <React.Fragment></React.Fragment>;
+};
+
+export default Bookstore;
